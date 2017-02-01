2d384e261385344f0d016a9867b151f47bd1c59d
Bryan Gmyrek <bryangmyrek@gmail.com>
2017-01-31 23:59:17 -0700

    Update build-boost.bat

--- build-boost.bat
+++ build-boost.bat
@@ -1,4 +1,4 @@
-rem bat file to build boost - mainly, it sets up the path for you
+rem experimental bat file to build boost
 
 set PATH=C:\mingw32\bin;%PATH%
 
@@ -8,4 +8,4 @@ bootstrap.bat mingw
 
 b2 --build-type=complete --with-chrono --with-filesystem --with-program_options --with-system --with-thread toolset=gcc variant=release link=static threading=multi runtime-link=static stage
 
-rem if b2 doesn't run, grab the command from this file and run it in the windows cmd.exe prompt
+rem if b2 doesn't run, grab the command from this file and run it...
